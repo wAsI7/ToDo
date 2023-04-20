ca8776953c562da6a5e35ad8f46eb02bfe796a13
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-04-20 19:09:41 +0530

    Style Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import "./style.css"
+
 export default function App() {
 
-  return "Hi"
+  return (
+    <form className= "new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+    </form>
+  )
 }
