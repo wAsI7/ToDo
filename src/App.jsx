d0810f21e626faeb0d32697fcfb5842b0d0274ed
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-04-20 20:09:47 +0530

    Logic Coded

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,26 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false}
       ]
     })
+
+    setNewItem("")
+  }
+  
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id ) {
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
@@ -34,14 +54,17 @@ export default function App() {
     </form>
     <h1 className="header">ToDo List</h1>
     <ul className="list">
+      {todos.length === 0 && "No Todos"}
       {todos.map(todo => {
         return (
-          <li>
+          <li key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed} />
+              <input type="checkbox" checked={todo.completed} 
+              onChange={e => toggleTodo(todo.id, e.target.checked)}
+              />
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
           </li>
         )
       })}
